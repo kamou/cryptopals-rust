2de2b6bb8c581a9b2ba3f06a443b1b5a035e1bdb
Ayman Khamouma <kamou.k@gmail.com>
2020-01-25 04:59:41 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,17 @@ mod crypto_pals {
             .collect()
     }
 
+    pub fn common_start_size(s1: &Vec<u8>, s2: &Vec<u8>) -> Option<usize>{
+        let mut offset = 0;
+        for (c1, c2) in s1.iter().zip(s2) {
+            if c1-c2 != 0 {
+                return Some(offset);
+            }
+            offset+=1;
+        }
+        None
+    }
+
     pub enum CipherMode {
         ECB,
         CBC(Vec<u8>),
@@ -36,47 +47,60 @@ mod crypto_pals {
             }
         }
 
-        pub fn ecb_block_size_after_padding(&self, size: usize) -> usize {
+
+        pub fn find_data_size_for_leak(&self) -> Option<(usize, usize)> {
             let block_size = self.detect_block_size();
-            let mut data = vec![0x41u8; size];
-            let ciphertext = self.blackbox(&data).expect("Failed to encrypt");
-            let blocks: Vec<&[u8]> = ciphertext.chunks(block_size).collect();
-            let mut pval: Option<Vec<u8>> = None;
-            let mut i: u32 = 0;
+            let mut pvec: Option<Vec<u8>> = None;
+            let mut pcs: Option<usize> = None;
+            for i in 0..block_size {
+                let mut block = vec![0x41u8; 10 * block_size - i];
+                let encrypted = self.blackbox(&block).unwrap();
 
-            // find first and last consecutive equal chunks
-            let mut blocks = blocks.iter();
-            let mut first: Option<u32> = None;
-            let last = loop {
-                let current_block = match blocks.next() {
-                    Some(b) => b.to_vec(),
-                    None => break None,
+                match pvec {
+                    Some(v) => {
+                        let cs = common_start_size(&v, &encrypted);
+                        match pcs {
+                            Some(_cs) => {
+                                if cs.unwrap() != _cs {
+                                    return Some((block.len(), _cs));
+                                }
+                            },
+                            None => (),
+                        };
+                        pcs = Some(cs.unwrap());
+                    },
+                    _ => (),
                 };
-
-                match pval {
-                    Some(v) if (!(v == current_block) && (first != None))
-                        => {pval = Some(v); break Some(i);},
-                        Some(v) if ((v == current_block) && (first == None))
-                            => first = Some(i-1),
-                        _ => (),
-                };
-                pval = Some(current_block);
-                i+=1;
-            };
-
-            let pv = pval.unwrap().to_vec();
-            let last = last.expect("Can't find correct size");
-
-            // increase data size until last block is equal to previous block
-            loop {
-                data.push(0x41);
-                let ciphertext = self.blackbox(&data).unwrap();
-                let blocks: Vec<&[u8]> = ciphertext.chunks(block_size).collect();
-                if blocks[last as usize].to_vec() == pv {
-                    return data.len();
-                }
+                pvec = Some(encrypted.clone(), );
             }
+            None
         }
+
+        // This function assumes constant data (or no data at all) is prepended to our input.
+        // Only tested on CBC and EBC
+        pub fn universal_leak(&self) -> Option<Vec<u8>> {
+            let (psize, csize) = self.find_data_size_for_leak().expect("Can't leak.");
+            println!("psize: {}, csize: {}", psize, csize);
+            let block_size = self.detect_block_size();
+            let mut pad = vec![0x41u8; psize];
+            let mut data = vec![0x41u8; psize];
+            let mut h: collections::HashMap<Vec<u8>, u8>;
+
+            loop {
+                h = self.last_byte_map(&data);
+
+                let encrypted = self.blackbox(&pad).unwrap();
+                pad.pop();
+                match h.get(&encrypted[csize - block_size..csize]) {
+                    None => break,
+                    Some(v) => data.push(*v),
+                };
+                data = data[1..].to_vec();
+
+            }
+            Some(data)
+        }
+
 
         pub fn detect_block_size(&self) -> usize {
             let mut pval: Option<usize> = None;
@@ -92,14 +116,68 @@ mod crypto_pals {
             };
         }
 
-        pub fn ecb_last_byte_map(&self, data: &Vec<u8>) -> collections::HashMap<Vec<u8>, u8> {
+        pub fn is_ecb(&self) -> bool{
+            let block_size = self.detect_block_size();
+            let data = vec![0x41u8;block_size*20];
+            let ciphertext = self.blackbox(&data).expect("Failed to encrypt");
+            let blocks: Vec<&[u8]> = ciphertext.chunks(block_size).collect();
+            for (i, b) in blocks.iter().enumerate() {
+                if i > 0 {
+                    if blocks[i-1] == *b {
+                        return true;
+                    }
+                }
+            }
+            return false;
+
+        }
+
+        pub fn ecb_find_prepend_size(&self) -> usize {
+            if !self.is_ecb() {
+                panic!("not an ecb cipher.");
+            }
+
+            let block_size = self.detect_block_size();
+            let data = vec![0x41u8;block_size*20];
+            let ciphertext = self.blackbox(&data).expect("Failed to encrypt");
+            let blocks: Vec<&[u8]> = ciphertext.chunks(block_size).collect();
+            let mut offset = None;
+            for (i, b) in blocks.iter().enumerate() {
+                if i > 0 {
+                    if blocks[i-1] == *b {
+                        println!("{:?}\n{:?}", &ciphertext[(i-1)*block_size..(i)*block_size], b);
+                        offset = Some((i-1)*block_size);
+                        break
+                    }
+                }
+            }
+
+            let offset = offset.unwrap();
+            let pattern = &ciphertext[offset..offset+16];
+            let data = vec![0x41u8; block_size];
+            let mut n = 1;
+            while n < block_size {
+                let mut random_bytes = random(n);
+                random_bytes.extend_from_slice(&data[..]);
+                let result = self.blackbox(&random_bytes).unwrap();
+                let mut i = 0;
+                if &result[offset..offset+16] == pattern {
+                    return offset - 16 + block_size - n;
+                }
+                n += 1;
+            }
+            0
+        }
+
+        pub fn last_byte_map(&self, data: &Vec<u8>) -> collections::HashMap<Vec<u8>, u8> {
+            let (psize, csize) = self.find_data_size_for_leak().unwrap();
             let block_size = self.detect_block_size();
             let mut h: collections::HashMap<Vec<u8>, u8> = collections::HashMap::new();
             for i in 0..256 {
-                let mut current = data[1..].to_vec();
+                let mut current = data.clone();
                 current.push(i as u8);
                 let k = self.blackbox(&current).expect("Failed to encrypt");
-                h.insert(k[current.len() - 1 - block_size..current.len() - 1].to_owned(), i as u8);
+                h.insert(k[csize - block_size..csize].to_owned(), i as u8);
             }
             h
         }
@@ -321,24 +399,18 @@ mod crypto_pals {
 use crypto_pals::{Cipher, Algo, CipherMode, Attacker};
 use rand::Rng;
 
-fn black_box_unknown_key(user_input: &Vec<u8>, block_size: usize) -> Result<Vec<u8>, &'static str> {
-    let key = crypto_pals::random(block_size);
-    let data = user_input.clone();
-
-    black_box_with_key(&data, &key, block_size)
-}
-
 static RAND_DATA: OnceCell<Vec<u8>> = OnceCell::new();
 static KEY: OnceCell<Vec<u8>> = OnceCell::new();
 
-fn black_box_with_key(user_input: &Vec<u8>, key: &Vec<u8>, block_size: usize) -> Result<Vec<u8>, &'static str> {
+fn black_box(user_input: &Vec<u8>) -> Result<Vec<u8>, &'static str> {
     static UNKNOWN_B64: &str = "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK";
     let mut unknown_str = base64::decode(&UNKNOWN_B64).unwrap();
+    let key = KEY.get().unwrap();
     let mut data = user_input.clone();
     let mut rng = rand::thread_rng();
     let mut prepad = match RAND_DATA.get() {
         None => {
-            let rd = crypto_pals::random(rng.gen_range(5,11) as usize);
+            let rd = crypto_pals::random(rng.gen_range(17,31) as usize);
             RAND_DATA.set(rd.clone()).expect("Could not set RAND_DATA");
             rd
         },
@@ -348,49 +420,19 @@ fn black_box_with_key(user_input: &Vec<u8>, key: &Vec<u8>, block_size: usize) ->
     data.append(&mut unknown_str);
     prepad.append(&mut data);
 
-    let mut ct = Cipher::new(Algo::AES(CipherMode::ECB, block_size));
+    let mut ct = Cipher::new(Algo::AES(CipherMode::ECB, 16));
     ct.set_key(key);
-    // ct.set_blackbox(&black_box);
     ct.encrypt(&prepad)
-}
-fn black_box(user_input: &Vec<u8>) -> Result<Vec<u8>, &'static str> {
-    let key = KEY.get().unwrap();
-    black_box_with_key(&user_input, &key, 16)
 }
 
 fn challenge_14(at: &Attacker) {
-    let block_size = at.detect_block_size();
-    println!("block size: {}", block_size);
-
-    let data_size = at.ecb_block_size_after_padding(block_size * 10);
-    let mut pad = vec![0x41u8; data_size];
-    let mut data = vec![0x41u8; data_size];
-    let mut h: collections::HashMap<Vec<u8>, u8>;
-
-    loop {
-        h = at.ecb_last_byte_map(&data);
-        pad.pop();
-
-        let encrypted = at.blackbox(&pad).unwrap();
-        match h.get(&encrypted[data_size - 1 - block_size..data_size-1]) {
-            None => break,
-            Some(v) => data.push(*v),
-        };
-        data = data[1..].to_vec();
-
-    }
-    println!("{}", String::from_utf8(data).unwrap());
+    let leak = at.universal_leak().unwrap();
+    println!("leak: {}", String::from_utf8(leak).unwrap());
 }
 
 fn main (){
     let key = crypto_pals::random(16);
     KEY.set(key.to_vec()).expect("Could not set KEY");
-
     let mut at = Attacker::new(&black_box);
-    let block_size = at.detect_block_size();
-
-    // let mut ct = Cipher::new(Algo::AES(CipherMode::ECB, block_size));
-    // ct.set_key(&key);
-    // at.set_blackbox(&black_box);
     challenge_14(&at);
 }
